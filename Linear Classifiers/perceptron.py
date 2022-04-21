d46d78e4588780b4cd278b6db2b64b56bd13f15f
Piraka Li <xianglol@umich.edu>
2022-04-21 18:48:16 -0400

    change offset impl

--- Linear Classifiers/perceptron.py
+++ Linear Classifiers/perceptron.py
@@ -12,11 +12,14 @@ def prepare_data(filename, x_cols, y_col, ratio=0.8, random_state=42):
     return X_train, y_train, X_test, y_test
 
 
-def perceptron_accuracy(X, y, weights, offset=0):
+def perceptron_accuracy(X, y, weights, offset_enabled=False):
     """
     acc = 1/n * sum(y_i != sign(w^T x_i))
     """
-    acc = ((y * (weights @ X.T + offset)) > 0).mean()
+    X_ = X
+    if offset_enabled:
+        X_ = np.hstack((np.ones((X.shape[0], 1)), X))
+    acc = ((y * (weights @ X_.T)) > 0).mean()
     return acc
 
 
@@ -25,29 +28,29 @@ def perceptron_algorithm(X, y, offset_enabled=False, max_iter=1000):
     X: (n, m)
     y: (n, )
     """
-    n, m = X.shape
+    X_ = X
+    if offset_enabled:
+        X_ = np.hstack((np.ones((X.shape[0], 1)), X)) # [1, x_1, x_2, ..., x_m]
+    n, m = X_.shape
     num_iter = 0
     weights = np.zeros(m)
-    offset = 0
     acc = 0
     while acc < 1 and num_iter < max_iter:
         for i in range(n):
-            if y[i] * (weights @ X[i].T + offset) <= 0:
-                weights = weights + y[i] * X[i]
-                if offset_enabled:
-                    offset = offset + y[i]
+            if y[i] * (weights @ X_[i].T) <= 0:
+                weights = weights + y[i] * X_[i]
                 num_iter += 1
-        acc = perceptron_accuracy(X, y, weights, offset)
+        acc = perceptron_accuracy(X, y, weights, offset_enabled)
         # print(f"iter: {num_iter}, acc: {acc}")
-    return weights, offset
+    return weights
 
 
 def evaluate(filename, features, y_col, offset_enabled=False, max_iter=1000):
     print("==========================")
     X_train, y_train, X_test, y_test = prepare_data(filename, features, y_col)
-    weights, offset = perceptron_algorithm(X_train, y_train, offset_enabled, max_iter)
-    train_acc = perceptron_accuracy(X_train, y_train, weights, offset)
-    test_acc = perceptron_accuracy(X_test, y_test, weights, offset)
+    weights = perceptron_algorithm(X_train, y_train, offset_enabled, max_iter)
+    train_acc = perceptron_accuracy(X_train, y_train, weights, offset_enabled)
+    test_acc = perceptron_accuracy(X_test, y_test, weights, offset_enabled)
     print(f"Model: {y_col} = {'w_0 + ' if offset_enabled else ''}{' + '.join([f'w_{i+1} * {x_cols[i]}' for i in range(len(x_cols))])}")
     print(f"Train accuracy: {train_acc}")
     print(f"Test accuracy: {test_acc}")
---
+++ Linear Classifiers/sgd.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+
+def stochastic_gradient_descent(X, y, lr=1e-3, max_iter=1000):
+    """
+    X: (n, m)
+    y: (n, )
+    """
+    n, m = X.shape
+    weights = np.zeros(m)
+    num_iter = 0
+    while num_iter < max_iter:
+        for i in range(n):
+            weights = weights + lr * y[i] * X[i]
+            num_iter += 1
+    return weights
